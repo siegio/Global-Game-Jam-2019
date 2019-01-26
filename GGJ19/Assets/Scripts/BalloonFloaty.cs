855a1a4e45d3dc62143455bd8dd70fb26c7845f6
SofieGB <40756356+SofieGB@users.noreply.github.com>
2019-01-26 12:16:17 +0100

    balloon now goes up very cool

--- GGJ19/Assets/Scripts/BalloonFloaty.cs
+++ GGJ19/Assets/Scripts/BalloonFloaty.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class BalloonFloaty : MonoBehaviour
 {
+	private float timePassed = 0f;
+	private Vector3 originalPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		originalPosition = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-		transform.position = transform.position + new Vector3(0, 0.1f, 0);
+    void Update() {
+		if (transform.position.y > 200) {
+			Destroy(gameObject);
+		}
+		timePassed += Time.deltaTime * 2f;
+		//-1 / (x + 1) + 1 + ((x / 2) ^ 5)
+		transform.position = originalPosition + new Vector3(0, (-1 / (timePassed + 1) + 1) * 2 + (Mathf.Pow(timePassed / 1.5f, 5)), 0);
     }
 }
