addc0009f2ca77f624d065b82f5dbb694fe9e5e2
SofieGB <40756356+SofieGB@users.noreply.github.com>
2019-01-26 14:36:18 +0100

    cleanupsies and also electricity kill

---
+++ GGJ19/Assets/Scripts/KillElectricityButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class KillElectricityButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+		(gameObject.GetComponent(typeof(Button)) as Button).onClick.AddListener(() => { Debug.Log("bye!");  Application.Quit(); });
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
