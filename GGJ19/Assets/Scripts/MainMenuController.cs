0d74733da45331e013d3423256ce176ac44633bb
SofieGB <40756356+SofieGB@users.noreply.github.com>
2019-01-26 20:06:24 +0100

    making a menu

---
+++ GGJ19/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+	public Button startGameButton;
+
+    // Start is called before the first frame update
+    void Start() {
+		startGameButton.onClick.AddListener(() => { SceneManager.LoadScene("MainMenuScene", LoadSceneMode.Single); });
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
