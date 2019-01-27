5b8fb2657932d2dd6d7da22e5f6ccd96b0c85de0
Josien Vos <josien.vos@outlook.com>
2019-01-27 14:35:21 +0100

    added particle to first batch of throwable objects

---
+++ GGJ19/Assets/Scripts/ParticleBurst.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBurst : MonoBehaviour
+{
+    
+	public ParticleSystem particle;
+	
+	private void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "HouseCollider"){
+			//particle.Clear();
+			particle.time = 0;
+			particle.Play();
+			Debug.Log("should do particle");
+		}
+	}
+	
+}
